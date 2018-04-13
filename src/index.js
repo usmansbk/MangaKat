2596d4e473034f0ef5d30879acdd0620a13ff1cb
usmansbk <usmansbk@gmail.com>
2018-04-13 22:04:35 +0100

    Remove log messages

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const middlewares = [
 	thunkMiddleware
 ];
 
-(process.env.NODE_ENV === 'development') && middlewares.push(loggerMiddleware);
+process.env.NODE_ENV || middlewares.push(loggerMiddleware);
 const store = createStore(
 	mangakatApp,
 	persistedState,
