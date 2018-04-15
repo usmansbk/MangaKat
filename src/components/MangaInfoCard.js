814fb5e07f64d34a945a581275f4a05b97b06f4c
usmansbk <usmansbk@gmail.com>
2018-04-15 02:52:11 +0100

    Remove download button

--- src/components/MangaInfoCard.js
+++ src/components/MangaInfoCard.js
@@ -76,7 +76,6 @@ class MangaInfoCard extends React.Component {
 				<section className='center-align'>
 					<Button onClick={this.onClick} name='resume'><Icon position='right' name='play_arrow' />{ isStart?'Start':'Resume'}</Button>
 					<Button onClick={this.onClick} name='favorite'><Icon position='right' name={'favorite' + (isFavorite?'':'_border')}/>Favorite</Button>
-					<Button style={{margin: '5px'}}onClick={this.onClick} className='disabled' name='download'><Icon position='right' name={'save'}/>Save Offline</Button>
 				</section>
 				<section>
 					<ul className="collapsible">
